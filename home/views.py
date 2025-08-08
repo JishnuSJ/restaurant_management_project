7a73e524b0b4e40edd671af55ec00259e9bbe599
Jishnu S. Jyothish <jishnujyothish316@gmail.com>
2025-08-08 09:52:30 +0530

    23315

--- home/views.py
+++ home/views.py
@@ -5,3 +5,6 @@ def homepage(request):
     restaurant = Restaurant.objects.first()
     restaurant_name = restaurant.restaurant_name if restaurant else "Restaurant"
     return render(request,'Homepage.html',{'restaurant_name':restaurant_name})
+
+def about_page(request):
+    return render(request,'about.html')
