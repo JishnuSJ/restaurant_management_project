b1c461c55a71dde726c0419709d07d1229757a44
Jishnu S. Jyothish <jishnujyothish316@gmail.com>
2025-08-21 09:39:54 +0530

    36309

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-class UserProfile(Models.Model)
+class UserProfile(Models.Model):
+    user = models.OnetoOneField(USer,on_delete=models.CASCADE,related_name='profile')
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"{self.name}({self.user.username})"
