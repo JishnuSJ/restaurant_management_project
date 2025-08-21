88426b4874443bf56db6defd2eb640821c536215
Jishnu S. Jyothish <jishnujyothish316@gmail.com>
2025-08-21 10:04:13 +0530

    36312

--- home/models.py
+++ home/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class UserProfile(Models.Model):
+class UserProfile(models.Model):
     user = models.OnetoOneField(USer,on_delete=models.CASCADE,related_name='profile')
     name = models.CharField(max_length=100)
     email = models.EmailField()
@@ -8,3 +8,19 @@ class UserProfile(Models.Model):
 
     def __str__(self):
         return f"{self.name}({self.user.username})"
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100)
+    items = models.ManytoManyField(Menu)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order by {self.customer_name} on {self.created_at.strftime('%y-%m-%d')}"
