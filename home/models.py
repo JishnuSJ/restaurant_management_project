ab4a8f200ca34dfdcc5001b23465066152b7b288
Jishnu S. Jyothish <jishnujyothish316@gmail.com>
2025-09-25 11:42:56 +0530

    51247

--- home/models.py
+++ home/models.py
@@ -28,7 +28,7 @@ class Order(models.Model):
     customer_name = models.CharField(max_length=100)
     items = models.ManytoManyField(Menu)
     total_amount = models.DecimalField(max_digits=8,decimal_places=2)
-    status = models.CharField(max_length=10,choice=ORDER_STATUS_CHOICES,default="PENDING")
+    #status = models.CharField(max_length=10,choice=ORDER_STATUS_CHOICES,default="PENDING")
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
