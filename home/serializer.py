b2124baf0d61f1f4c5c0278d35dc0c8fcf36f871
Jishnu S. Jyothish <jishnujyothish316@gmail.com>
2025-09-25 11:46:36 +0530

    51247

--- home/serializer.py
+++ home/serializer.py
@@ -3,3 +3,10 @@ class MenuCategoryserializer(serializer.ModelSerializer):
         models=MenuCategory
         fields = ['id','name','description','price','category','is_available']
 
+class OrderSerilaizer(serializers.ModelSerializer):
+    items = MenuItemSerializer(many=True,read_only=True)
+    customer = serializers.CharField(source='customer.user.username')
+
+    class meta:
+        model = Order
+        fields = ['id','customer','items','total_price','created_at']
