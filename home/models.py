6a2b2abf7dc83a47ab93c09a3425a44e15c4a8fd
Jishnu S. Jyothish <jishnujyothish316@gmail.com>
2025-09-26 17:16:03 +0530

    51249

--- home/models.py
+++ home/models.py
@@ -5,7 +5,7 @@ class UserProfile(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     phone_number = models.CharField(max_length=15)
-
+    
     def __str__(self):
         return f"{self.name}({self.user.username})"
 
