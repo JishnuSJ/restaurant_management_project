bb166dd3b63837fda6fd9d0a4f70632300e0603f
Jishnu S. Jyothish <jishnujyothish316@gmail.com>
2025-10-26 17:40:27 +0530

    56332

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display=('name','address'.'phone','email','is_active')
+    serach_field = ('name','address')
+    list_filter = ('is_active',)
+admin.site.register(Restaurant,RestaurantAdmin)
