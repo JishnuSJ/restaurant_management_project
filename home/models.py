aca7ecd43289ff11c3345518916fbe04fc8d6f8e
Jishnu S. Jyothish <jishnujyothish316@gmail.com>
2025-10-26 17:31:52 +0530

    56329

--- home/models.py
+++ home/models.py
@@ -91,6 +91,7 @@ class Todayspecial(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6,decimal_places=6)
+    img = models.URLField(blank=True,null=True)
 
 
 class Cheff(models.Model):
